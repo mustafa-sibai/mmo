dc1d5b318146022c9836f391b46a40849a7d8c0d
Ilya Achmetov <ilya.achmetov@gmail.com>
2025-04-20 16:22:27 +0300

    added rendering of triangle

--- mmo/Core/Renderer/D3D11/D3D11Graphics.h
+++ mmo/Core/Renderer/D3D11/D3D11Graphics.h
@@ -10,6 +10,12 @@ private:
 	ID3D11DeviceContext* pContext = nullptr;
 	ID3D11RenderTargetView* pTarget = nullptr;
 
+	// Triangle rendering resources
+	ID3D11VertexShader* pVertexShader = nullptr;
+	ID3D11PixelShader* pPixelShader = nullptr;
+	ID3D11InputLayout* pInputLayout = nullptr;
+	ID3D11Buffer* pVertexBuffer = nullptr;
+
 public:
 	D3D11Graphics(const HWND& hWnd);
 	D3D11Graphics(const D3D11Graphics&) = delete;
@@ -17,4 +23,6 @@ public:
 	~D3D11Graphics();
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
+	void InitTriangle();
+	void DrawTestTriangle();
 };
